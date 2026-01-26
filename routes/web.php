45e240bef042a06b583b79a9c461ffc34d6cadb5
meetrajsinh120892-ui <meetrajsinh.120892@marwadiuniversity.ac.in>
2026-01-26 23:15:44 +0530

    Added the showregistration and showlogin routes

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,11 @@ Route::get('/testimonial', function () {
 
 // user authentication routes
 
-
+Route::get('/register', [AuthController::class, 'showRegister']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/login', [AuthController::class, 'showLogin'])
+    ->name('login'); 
 
 Route::post('/login', [AuthController::class, 'login']);
 
