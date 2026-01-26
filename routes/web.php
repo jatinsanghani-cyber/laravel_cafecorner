579eea70bc2c9d45ab01c08cd13a390e95da359b
meetrajsinh120892-ui <meetrajsinh.120892@marwadiuniversity.ac.in>
2026-01-26 23:19:51 +0530

    Updated the user authentiction routs.

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,6 @@ Route::get('/login', [AuthController::class, 'showLogin'])
     ->name('login'); 
 
 Route::post('/login', [AuthController::class, 'login']);
-
 Route::post('/logout', [AuthController::class, 'logout'])
     ->middleware('auth');
+
