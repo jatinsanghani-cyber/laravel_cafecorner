eeb0597bc221d1e674610081ad968ecbd2a594d3
jatinsanghani-cyber <jatinsanghanio7@gmail.com>
2026-01-27 11:03:34 +0530

    Added the reservation table

---
+++ database/migrations/2026_01_24_091246_create_reservations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('reservations', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('user_id')
+              ->constrained()
+              ->onDelete('cascade');
+
+        $table->string('phone', 15);
+        $table->date('date');
+        $table->time('time');
+        $table->unsignedTinyInteger('guests');
+        $table->string('seating');
+        $table->string('occasion')->nullable();
+        $table->text('notes')->nullable();
+
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
